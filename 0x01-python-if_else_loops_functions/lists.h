5dce4427e8b0d28ef408d07044fbc48062430973
AchiiBella <jeanokello1@gmail.com>
2022-11-30 22:55:07 -0800

    Python

--- 0x01-python-if_else_loops_functions/lists.h
+++ 0x01-python-if_else_loops_functions/lists.h
@@ -9,6 +9,7 @@
  * @next: points to the next node
  *
  * Description: singly linked list node structure
+ *
  */
 typedef struct listint_s
 {
@@ -22,6 +23,7 @@ void free_listint(listint_t *head);
 listint_t *insert_node(listint_t **head, int number);
 def remove_char_at(str, n)
 def fizzbuzz():
+
 listint_t *insert_node(listint_t **head, int number);
 
 #endif /* LISTS_H */
